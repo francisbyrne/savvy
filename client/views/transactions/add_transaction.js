1ebf48fbef17e019f6bf999b875bfa77ffe05837
francisbyrne <byrnify@gmail.com>
2014-04-29 04:00:29 +0300

    hmmm

---
+++ client/views/transactions/add_transaction.js
@@ -0,0 +1,24 @@
+Template.add_transaction.rendered = function(){
+    // initialize typeahead for search
+    Meteor.typeahead(this.$('.ticker'));
+};
+
+Template.add_transaction.search = function(){
+  // map symbols to array for typeahead datasource
+  return Stocks.find().fetch().map(function(it){ return it.symbol; });
+};
+
+Template.add_transaction.events({
+  'submit form#add-transaction': function(event, template) {
+    event.preventDefault();
+
+    var form = template.find('#add-transaction');
+    var fields = forms.parseForm(form);
+
+    transact.addTransaction(fields, function() {
+      // Reset form and focus input
+      form.reset();
+      template.find("input[name=symbol]").focus();
+    })
+  }
+});
