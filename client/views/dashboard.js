3e0331caf4767f89e75e15e2c65ef52611efdd76
francisbyrne <byrnify@gmail.com>
2014-06-28 16:51:10 +0100

    added total summary to dashboard

--- client/views/dashboard.js
+++ client/views/dashboard.js
@@ -2,6 +2,7 @@ Template.dashboard.helpers({
   losers: function() {
     return Holdings.find({'userId': Meteor.userId()}, {'sort': {'daysGain': 1}, 'limit': 3}).fetch();
   },
+  total: totalHoldings,
   winners: function() {
     return Holdings.find({'userId': Meteor.userId()}, {'sort': {'daysGain': -1}, 'limit': 3}).fetch();
   }
