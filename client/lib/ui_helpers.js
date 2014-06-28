ea171e8be5a9c3d08fadd37ec598e1197a3801ce
francisbyrne <byrnify@gmail.com>
2014-06-28 17:41:49 +0100

    formatted dashboard nicer

--- client/lib/ui_helpers.js
+++ client/lib/ui_helpers.js
@@ -1,5 +1,24 @@
+var gainOrLoss = function(check, field) {
+  if (! field) {
+    field = check;
+  }
+
+  if (check > 0)
+    return '<span class="gain">' + field + '</span>';
+  else if (check < 0)
+    return '<span class="loss">' + field + '</span>';
+  else
+    return field;
+};
+
 formatPercent = function formatPercent(field) {
-  return numeral(field || 0).format('+0.00%');
+  var percent = numeral(field || 0).format('+0.00%');
+  return gainOrLoss(field, percent);
+};
+
+formatPercentBrackets = function formatPercentBrackets(field) {
+  var percent = '(' + numeral(field || 0).format('+0.00%') + ')';
+  return gainOrLoss(field, percent);
 };
 
 formatCurrency = function formatCurrency(field) {
@@ -9,11 +28,7 @@ formatCurrency = function formatCurrency(field) {
 formatCurrencySign = function formatCurrencySign(field) {
   // Workaround for Numeral.js issue #89: $+0.00 instead of +$0.00
   var amount = (field > 0 ? '+' : '') + numeral(field || 0).format('$0,0.00');
-  if (field > 0)
-    amount = '<span class="gain">' + amount + '</span>';
-  else if (field < 0)
-    amount = '<span class="loss">' + amount + '</span>';
-  return amount;
+  return gainOrLoss(field, amount);
 };
 
 formatDate = function formatDate(field) {
@@ -21,6 +36,7 @@ formatDate = function formatDate(field) {
 };
 
 UI.registerHelper('formatPercent', formatPercent);
+UI.registerHelper('formatPercentBrackets', formatPercentBrackets);
 UI.registerHelper('formatCurrency', formatCurrency);
 UI.registerHelper('formatCurrencySign', formatCurrencySign);
 UI.registerHelper('formatDate', formatDate);
