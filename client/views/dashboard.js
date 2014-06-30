aa9fd3bd6acdf9b565981f4931ed808a9f69ac72
francisbyrne <byrnify@gmail.com>
2014-06-30 15:34:42 +0100

    changed font from oswald to roboto

--- client/views/dashboard.js
+++ client/views/dashboard.js
@@ -1,10 +1,10 @@
 Template.dashboard.helpers({
   losers: function() {
-    return Holdings.find({'userId': Meteor.userId()}, {'sort': {'daysGain': 1}, 'limit': 3}).fetch();
+    return Holdings.find({'userId': Meteor.userId(), 'daysGain': {$lt: 0}}, {'sort': {'daysGain': 1}, 'limit': 3}).fetch();
   },
   total: totalHoldings,
   winners: function() {
-    return Holdings.find({'userId': Meteor.userId()}, {'sort': {'daysGain': -1}, 'limit': 3}).fetch();
+    return Holdings.find({'userId': Meteor.userId(), 'daysGain': {$gt: 0}}, {'sort': {'daysGain': -1}, 'limit': 3}).fetch();
   },
   holdingsExist: function() {
     return Holdings.find({'userId': Meteor.userId()}).count() > 0;
